e15011d613cadb4aee20aabc32098cc45bfd8ee0
StevenMSS <stevenmourass@gmail.com>
2024-12-03 20:03:18 -0300

    Merge pull request #1 from StevenMSS/SharedPreferences_BuscarDados
    Shared preferences Começando SplashActivity.

--- app/src/main/java/View/SplashActivity.java
+++ app/src/main/java/View/SplashActivity.java
@@ -15,6 +15,11 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        comutarTelaSplash;
+    }
+
+    private void cumutarTelaSplash(){
+
     }
 
 }
